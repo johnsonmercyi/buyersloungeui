4f8898802393f898663605bc94dbb3b19213be52
johnsonmercyi <50102624+johnsonmercyi@users.noreply.github.com>
2024-01-08 00:02:38 +0100

    confirm commit and push

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,44 +2,35 @@ document.addEventListener('DOMContentLoaded', function (event) {
   const usernameField = document.querySelector("#username");
   const passwordField = document.querySelector("#password");
   const submitButton = document.querySelector("#loginButton");
+  const usernameErrorDisplay = document.querySelector(".username-wrapper .error-message");
 
-  // Traditional Javascript function
-  function doSomething() {
-    // Using the this keyword here would have an issue
-    return "[doSomething] Called!";
-  }
+  submitButton && submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const validateReport = validateFields(usernameField, passwordField);
 
-  // Javascript arrow function
-  const arrowFunction = () => {
-    // Using the this keyword here would refer to the object of the document which is what we want
-  }
+    for(const report of validateReport) {
+      if (report.field === "username") {
+        usernameErrorDisplay.innerHTML = report.errorMessage;
+      }
+    }
 
-  submitButton.addEventListener('click', (event) => {
-    event.preventDefault();// Stops the form default behaviour
-    const car = new Car(usernameField.value, passwordField.value);
-    // car.submitDetails();
 
-    car.executeMethods(doSomething);
+
+    console.log(validateReport);
   });
   
+  function validateFields(...fields) {
+    const fieldReport = [];
+    for (const field of fields) {
+      if (!field.value) {
+        fieldReport.push({
+          field: field.getAttribute("name"),
+          error: true,
+          errorMessage: `${field.getAttribute("name")} is required.`
+        });
+      }
+    }
+    return fieldReport;
+  }
 });
-
-// Demonstrates Javascript classes
-class Car {
-
-  constructor(username, password) {
-    this.username = username;
-    this.password = password;
-  }
-
-  submitDetails = () => {
-    console.log("Submitting details...");
-    console.log("Name: ", this.username);
-    console.log("Age: ", this.password);
-  }
-
-  executeMethods = (method) => {
-    console.log(method);
-  }
-}
 
