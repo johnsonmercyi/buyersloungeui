14e6820c69b7a270832edc2e490dabab48e06b06
johnsonmercyi <johnsonmercy.ng@gmail.com>
2024-01-21 01:24:04 +0100

    started validating the signup form fields

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,3 +1,58 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  
+import { validateFields } from './util/util.mjs';
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  const firstnameField = document.querySelector(".form-field#firstname");
+  const lastnameField = document.querySelector(".form-field#lastname");
+  const usernameField = document.querySelector(".form-field#username");
+  const passwordField = document.querySelector(".form-field#password");
+  const confirmPasswordField = document.querySelector(".form-field#confirmPassword");
+  const emailField = document.querySelector(".form-field#email");
+  const genderSelect = document.querySelector(".form-field#gender");
+  const dobField = document.querySelector(".form-field#dateOfBirth");
+  const addressField = document.querySelector(".form-field#address");
+  const nationalityField = document.querySelector(".form-field#nationality");
+  const customerRadio = document.querySelector(".form-field#customer");
+  const sellerRadio = document.querySelector(".form-field#seller");
+
+  const submitBtn = document.querySelector(".form-field#submit");
+
+  submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const validateReport = validateFields(
+      firstnameField, 
+      lastnameField, 
+      usernameField, 
+      passwordField, 
+      confirmPasswordField,
+      email,
+      genderSelect,
+      dobField,
+      addressField,
+      nationalityField
+    );
+
+    console.log(validateReport);
+
+    // let userType = "n/a";
+    // if (customerRadio.checked) {
+    //   userType = customerRadio.value;
+    // } else if (sellerRadio.checked) {
+    //   userType = sellerRadio.value;
+    // }
+
+    // console.log({
+    //   firstname: firstnameField.value,
+    //   lastname: lastnameField.value,
+    //   username: usernameField.value,
+    //   password: passwordField.value,
+    //   confirmPassword: confirmPasswordField.value,
+    //   email: emailField.value,
+    //   gender: genderSelect.value, // Doubtful yet!
+    //   dob: dobField.value,
+    //   address: addressField.value,
+    //   nationality: nationalityField.value,
+    //   userType: userType,
+    // });
+  });
 });
--- public/javascript/util/util.mjs
+++ public/javascript/util/util.mjs
@@ -2,36 +2,46 @@ function validateFields(...fields) {
 
   return fields.map(field => {
     // Check if the field is filled
-    if (!field.value) {
-      return {
-        field: field.getAttribute("name"),
-        error: true,
-        errorMessage: `${field.getAttribute("name")} is required.`,
-        index: fields.indexOf(field)
-      };
-    } else {
-      // When the field is filled
+    if (field.type === "text" ||
+      field.type === "password" ||
+      field.type === "date" ||
+      field.type === "email" ||
+      field.type === "textarea" ||
+      field.nodeName === "SELECT") {
+      if (!field.value) {
+        return {
+          field: field.getAttribute("name"),
+          error: true,
+          errorMessage: `${field.getAttribute("name")} is required.`,
+          index: fields.indexOf(field)
+        };
+      } else {
+        // When the field is filled
 
-      // Checks if the password is contains up to 8 characters
-      if (field.getAttribute("name") === "password") {
-        if (field.value.length < 8) {
-          return {
-            field: field.getAttribute("name"),
-            error: true,
-            errorMessage: `${field.getAttribute("name")} is must be at least 8 characters`,
-            index: fields.indexOf(field)
-          };
+        // Checks if the password is contains up to 8 characters
+        if (field.getAttribute("name") === "password") {
+          if (field.value.length < 8) {
+            return {
+              field: field.getAttribute("name"),
+              error: true,
+              errorMessage: `${field.getAttribute("name")} is must be at least 8 characters`,
+              index: fields.indexOf(field)
+            };
+          }
         }
-      }
 
-      // Yeaa! Everything is okay.
-      return {
-        field: field.getAttribute("name"),
-        error: false,
-        errorMessage: ``,
-        index: fields.indexOf(field)
-      };
+        // Yeaa! Everything is okay.
+        return {
+          field: field.getAttribute("name"),
+          error: false,
+          errorMessage: ``,
+          index: fields.indexOf(field)
+        };
+      }
+    } else {
+      // Check for other form fields that the validation does not match with that of the input, select etc...
     }
+
   });
 }
 
